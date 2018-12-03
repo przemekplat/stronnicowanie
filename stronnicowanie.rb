c9870dce67bedec12d3161d72e8b136e296a0aa0
Przemek P <45264375+przemekplat@users.noreply.github.com>
2018-12-03 05:27:23 +0100

    Update stronnicowanie.rb
    w pełni działający skrypt

--- stronnicowanie.rb
+++ stronnicowanie.rb
@@ -1,72 +1,128 @@
-def ElementyWstronach(iloscElementow,iloscStron) #obliczanie ile elementów znajduje się na stronie
- licz = 0.0
- pelnewiersze = 0
- elementyzapelniajace = 0
- licz = iloscElementow/iloscStron.to_f
- elementyzapelniajace = (licz-licz.to_i)*iloscStron
- return pelnewiersze = licz.to_i,elementyzapelniajace.round
+require 'io/console'
+require 'colorize'
+
+def user_input() #wartości wpisywane przez użytkownika
+  puts "Podaj zakres końcowy zbioru elementów (od 0 do x):"
+  user_elements_count = gets.chomp.to_i
+  puts "Na ile stron podzielić zbiór elementów:"
+  user_page_count = gets.chomp.to_i
+  puts "Podaj liczbę numerów indeksów (ile stron ma się wyświetlać na dole):"
+  user_index_digits_count = gets.chomp.to_i
+  user_index_digits_count = user_page_count if user_page_count < user_index_digits_count
+  [user_elements_count, user_page_count, user_index_digits_count]
 end
 
-def StrzalkiLewo(aktywnaStrona,poczatek) #wypisuje strzalki w lewo przy indeksie
-  if aktywnaStrona+poczatek >= 1 then
-    if aktywnaStrona+poczatek >= 2 then
-    print "<< < "
-  else
-    print " < "
-  end
-end
+def calculate_elements_on_page(user_elements_count,user_page_count) #obliczanie ile elementów znajduje się na stronie
+  elements = 0.0
+  additional_element_if_uneven = 0
+  elements = user_elements_count/user_page_count.to_f
+  additional_element_if_uneven = (elements-elements.to_i)*user_page_count #dodatkowe wiersze jeżeli liczba elementów jest niepodzielna przez ilość stron
+  [elements.to_i,additional_element_if_uneven.round]
 end
 
-def StrzalkiPrawo(aktywnaStrona,koniec,iloscStron) #wypisuje strzalki w prawo przy indeksie
-  if aktywnaStrona+koniec <= iloscStron-1 then
-    print ">"
-    print " >>" if aktywnaStrona+koniec <= iloscStron-2
-end
-end
-
-def WypisStrony(ileStronIndeks,aktywnaStrona,poczatek)
-  wypis = 0 #ile razy wypisaliśmy indeks
-  while ileStronIndeks > wypis do
-    if 0 <= (aktywnaStrona+poczatek) then
-      print "#{aktywnaStrona+poczatek} "
-      wypis += 1
+def print_arrows_right(active_page,left_offset_index) #wypisuje strzalki w lewo przy indeksie
+  if active_page+left_offset_index >= 1 then
+    if active_page+left_offset_index >= 2 then
+      print "<< < "
+    else
+      print "< "
     end
-   poczatek += 1
   end
 end
 
-#puts "Podaj ilość elementów:"
-#iloscElementow = gets.chomp.to_i
-iloscElementow = 75
-#puts "Podaj ilość stron:"
-#iloscStron = gets.chomp.to_i
-iloscStron = 3
-#puts "Podaj ilość indeksów:"
-ileStronIndeks = 1
-wynik = ElementyWstronach(iloscElementow,iloscStron)
-puts "Podaj numer strony na który chcesz przejść:"
-aktywnaStrona = gets.chomp.to_i
- strony = Array.new(iloscStron)
- puts  # This also returns 20
- i = 0
- (strony.length).times {
-   strony[i] = wynik[0]
-   strony[i] += 1 if i < wynik[1]
-   i += 1
- }
- i = 1
-strony[aktywnaStrona].times{
-puts "To element numer #{i} ze strony #{aktywnaStrona}"
-i += 1
-}
-poczatek = -(ileStronIndeks/2) #o ile przesunięty jest początek indeksacji (numery na dole) względem wybranej strony
-koniec = (ileStronIndeks/2) #o ile przesunięty jest koniec indeksacji (numery na dole) względem wybranej strony
- if ileStronIndeks&1 != 0 then #dla parzystych indeksów koniec jest przesunięty o 1
-  koniec += (1)
+def print_arrows_left(active_page,right_offset_index,user_page_count,user_index_digits_count) #wypisuje strzalki w prawo przy indeksie
+  if active_page+right_offset_index <= user_page_count - 1 && user_page_count != user_index_digits_count then
+    print ">"
+    print " >>" if active_page+right_offset_index <= user_page_count -2 && user_page_count-user_index_digits_count >= 2
+  end
 end
-if (aktywnaStrona+koniec) > strony.length then # /komentarz do poprawy/ jeżeli nr wybranej strony + wartość przesunięcia na końcu indeksowania jest większa niż liczba wszystkich stron, wtedy...
-  poczatek -= (aktywnaStrona+koniec)-(strony.length) # ...przesuwamy początek o różnice między wybraną przez użytkownika stroną + wartością przesunięcia na początku a całkowitą iloścą stron
+
+def print_index_of_active_page(user_index_digits_count,active_page,left_offset_index) #wypisywanie indeksu
+  index_print_count = 0 #ile razy wypisaliśmy indeks
+  while user_index_digits_count > index_print_count
+    if 0 <= (active_page+left_offset_index)
+      if (active_page == (active_page+left_offset_index)) then print "#{(active_page+left_offset_index)} ".red else print "#{(active_page+left_offset_index)} " end
+      index_print_count += 1
+    end
+    left_offset_index += 1
+  end
 end
-StrzalkiLewo(aktywnaStrona,poczatek)
-WypisStrony(ileStronIndeks,aktywnaStrona,poczatek)
-StrzalkiPrawo(aktywnaStrona,koniec,iloscStron)
+
+
+def write_array(user_elements_count,user_page_count,elements_if_uneven,active_page,array)   #wpisywanie elementów do tablicy
+  i = 0
+  if elements_if_uneven > 0  #dodatkowe wiersze w przypadku gdy liczba elementow jest niepodzielna przez liczbę stron
+    while i < elements_if_uneven
+      array[i] << ""
+      i += 1
+    end
+  end
+  add_element_count = 0
+  (array.length).times do |j| #wpisujemy elementy do tablicy dwuwymiarowej
+    (array[j].length).times do |jj|
+      if add_element_count < user_elements_count
+        array[j][jj] = add_element_count+1
+        add_element_count += 1
+      end
+    end
+  end
+end
+
+def print_active_page(active_page,array) #wypisujemy zawartość aktywnej strony
+  if active_page > array.length || active_page < 0
+    puts "Nieprawidłowy indeks"
+  else
+    (array[active_page].length).times do |jj|
+      puts "Element '#{(array[active_page][jj])}'"
+    end
+  end
+end
+
+def set_begin_end(user_index_digits_count, active_page, array) #ustawiamy przesunięcie początku i końca indeksacji
+  left_offset_index = -(user_index_digits_count/2) #o ile przesunięty jest początek indeksacji (numery na dole) względem wybranej strony
+  right_offset_index = (user_index_digits_count/2) #o ile przesunięty jest koniec indeksacji (numery na dole) względem wybranej strony
+  right_offset_index += (1) if user_index_digits_count&1 != 0  #dla parzystych indeksów koniec jest przesunięty o 1
+  left_offset_index -= (active_page+right_offset_index)-(array.length) if active_page+right_offset_index > array.length # /komentarz do poprawy/ jeżeli nr wybranej strony + wartość przesunięcia na końcu indeksowania jest większa niż liczba wszystkich stron, wtedy przesuwamy początek o różnice między wybraną przez użytkownika stroną + wartością przesunięcia na początku a całkowitą iloścą stron.
+  [left_offset_index, right_offset_index]
+end
+
+user_elements_count,user_page_count,user_index_digits_count,active_page = user_input()
+elements_in_array, elements_if_uneven, active_page = calculate_elements_on_page(user_elements_count,user_page_count) #elements_in_array = elementy w tablicy, elements_if_uneven = dodatkowe elementy w tablicy gdy nierówne
+puts "Podaj numer strony na który chcesz przejść (wartość od 0 do #{user_page_count-1}):"
+active_page = gets.chomp.to_i
+array = Array.new(user_page_count) {Array.new(elements_in_array)}
+active_page = 0 if active_page > 0 || active_page < array.length
+write_array(user_elements_count,user_page_count,elements_if_uneven,active_page,array)
+print_active_page(active_page,array)
+left_offset_index, right_offset_index = set_begin_end(user_index_digits_count, active_page, array)
+print_arrows_right(active_page,left_offset_index)
+print_index_of_active_page(user_index_digits_count,active_page,left_offset_index)
+print_arrows_left(active_page,right_offset_index,user_page_count,user_index_digits_count)
+while true
+  user_input = $stdin.getch
+  case user_input
+  when 'q'
+    exit
+  else
+    case user_input
+    when ',', "<"
+      active_page -= 1 if active_page >= 1
+      puts "\n\nStrona numer #{active_page}\n\n"
+      print_active_page(active_page,array)
+      puts ""
+      left_offset_index, right_offset_index = set_begin_end(user_index_digits_count, active_page, array)
+      print_arrows_right(active_page,left_offset_index)
+      print_index_of_active_page(user_index_digits_count,active_page,left_offset_index)
+      print_arrows_left(active_page,right_offset_index,user_page_count,user_index_digits_count)
+    when '.', '>'
+      active_page += 1 if active_page < (array.length)-1
+      puts "\n\nStrona numer #{active_page}\n\n"
+      print_active_page(active_page,array)
+      puts ""
+      left_offset_index, right_offset_index = set_begin_end(user_index_digits_count, active_page, array)
+      print_arrows_right(active_page,left_offset_index)
+      print_index_of_active_page(user_index_digits_count,active_page,left_offset_index)
+      print_arrows_left(active_page,right_offset_index,user_page_count,user_index_digits_count)
+    end
+  end
+end
